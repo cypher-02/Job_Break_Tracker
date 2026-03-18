d72455c5133f2c1ad044f4311a0b6571777599b7
Parth singh sikarwar <105065211+cypher-02@users.noreply.github.com>
2026-03-18 05:53:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+let a = 30;
+
+// get stored value when page loads
+let storedValue = localStorage.getItem("a");
+
+if (storedValue !== null) {
+    a = JSON.parse(storedValue);
+}
+
+const main = document.querySelector(".main");
+const sumbit = document.querySelector(".sumbit");
+const exit_hours = document.querySelector(".exit_hours");
+const exit_minutes = document.querySelector(".exit_minutes");
+const enter_hours = document.querySelector(".enter_hours");
+const enter_minutes = document.querySelector(".enter_minutes");
+const reset = document.querySelector(".reset");
+
+//creating paragrapgh
+const startPara = document.createElement("p");
+main.append(startPara);
+
+// function to update text
+function updateBreak(){
+    startPara.textContent = "Remaining break " + a + " min";
+}
+
+// show stored value on page load
+updateBreak();
+
+sumbit.addEventListener('click', ()=>{
+    //values
+    let exit_h = exit_hours.value.trim();
+    let exit_min = exit_minutes.value.trim();
+    let enter_h = enter_hours.value.trim();
+    let enter_min = enter_minutes.value.trim();
+
+    //string conversion
+    let ent_h = parseFloat(exit_h);
+    let ent_min = parseFloat(exit_min);
+    let ext_h = parseFloat(enter_h);
+    let ext_min = parseFloat(enter_min);
+
+    //hours to min
+    let ent_h_to_min = ent_h * 60;
+    let ext_h_to_min = ext_h * 60;
+    
+    ent_h_to_min = ent_h_to_min + ent_min;
+    ext_h_to_min = ext_h_to_min + ext_min;
+
+    
+    a = a-(ext_h_to_min - ent_h_to_min);
+
+    // save value
+    localStorage.setItem('a', JSON.stringify(a));
+
+    // update text
+    updateBreak();
+});
+ 
+reset.addEventListener('click',()=>{
+    // reset value
+     a = 30;
+
+    localStorage.removeItem("a");
+
+    updateBreak();
+});
+
